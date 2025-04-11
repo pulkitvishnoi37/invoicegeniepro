6a227bad9fab14d21cd99fecb7418da0ea8e38b4
pulkitvishnoi37 <pulkitvishnoi37@gmail.com>
2025-04-11 18:53:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('InvoiceGenie App Loaded');
+document.getElementById('root').innerHTML = `
+  <h1>Welcome to InvoiceGenie</h1>
+  <p>This is your site homepage rendered via JavaScript!</p>
+`;
