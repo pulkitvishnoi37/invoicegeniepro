879d7d6b61d1a0a99af960d755b03cb80bee634d
pulkitvishnoi37 <pulkitvishnoi37@gmail.com>
2025-04-13 13:30:44 +0530

    Create word-to-pdf.js

---
+++ word-to-pdf.js
@@ -0,0 +1,2 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mammoth/1.4.2/mammoth.browser.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
