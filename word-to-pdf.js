69b6a5cd795efee65f7a9a71cc96194814cdc038
pulkitvishnoi37 <pulkitvishnoi37@gmail.com>
2025-04-13 21:22:18 +0530

    Delete word-to-pdf.js

--- word-to-pdf.js
+++
@@ -1,2 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mammoth/1.4.2/mammoth.browser.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
