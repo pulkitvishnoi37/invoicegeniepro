7064495957ccbaf7a50de500dd544c1ae0c6b279
pulkitvishnoi37 <pulkitvishnoi37@gmail.com>
2025-04-13 22:46:49 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+function createInvoice() {
+  alert("Invoice generation coming soon!");
+}
+
+function convertWordToPDF() {
+  alert("Word to PDF conversion feature is under development!");
+}
+
+function convertPDFToWord() {
+  alert("PDF to Word conversion feature is coming soon!");
+}
